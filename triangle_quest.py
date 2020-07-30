269c37e1e5aa70843db8045a21be14d374130099
elogs <elogs8@gmail.com>
2020-07-30 22:03:18 +0800

    Adding answer for Triangle Quest

---
+++ triangle_quest.py
@@ -0,0 +1,5 @@
+# https://www.hackerrank.com/challenges/python-quest-1/problem
+
+if __name__ = '__main__':
+    for i in range(1,int(input())):
+        print(i * (sum(map(lambda x: 10**x, list(range(1, i)))) + 1))
